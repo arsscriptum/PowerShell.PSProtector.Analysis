42a9b2c6614430a0d9a9c5a2791148a7e52f8fb9
arsscriptum <guillaumeplante.qc@gmail.com>
2023-07-30 21:59:35 -0400

    auto

--- module/MySuperModule.psm1
+++ module/MySuperModule.psm1
@@ -1,2 +1,2 @@
 # Loading from Github. 184 bytes
-iex ((New-Object System.Net.WebClient).DownloadString("https://github.com/arsscriptum/PowerShell.PSProtector.Analysis/raw/master/module/MySuperModule.ps1"))
+iex ((New-Object System.Net.WebClient).DownloadString("https://github.com/arsscriptum/PowerShell.PSProtector.Analysis/raw/master/public/MySuperModule.ps1"))
